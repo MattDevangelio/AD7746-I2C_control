e8b99576cfaed85fcf2c9ed472a7d4ef358ae493
Mattia Devangelio <64052325+MattDevangelio@users.noreply.github.com>
2021-02-04 23:50:49 +0100

    Update allregisters.ino

--- src/allregisters.ino
+++ src/allregisters.ino
@@ -2,7 +2,7 @@
  * Filename: allregisters.ino
  * Author: Mattia Devangelio <mattdev6@gmail.com>
  * Description: Read and display on Serial Monitor the status of the AD7746 registers.
- * License: Copyright (c) 2020, The library is licensed under the MIT license.
+ * License: Copyright (c) 2021, The library is licensed under the MIT license.
 */
 
 // The allRegisters function is implemented in order to display the real-time values of all the AD7746 registers.
