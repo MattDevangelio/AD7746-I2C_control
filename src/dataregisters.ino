36e30b36306a8b8e07d478d31d22035f2f3db5cb
Mattia Devangelio <64052325+MattDevangelio@users.noreply.github.com>
2021-02-04 10:46:37 +0100

    Update dataregisters.ino

--- src/dataregisters.ino
+++ src/dataregisters.ino
@@ -1,10 +1,15 @@
+/* 
+ * Filename: dataregisters.ino
+ * Author: Mattia Devangelio <mattdev6@gmail.com>
+ * Description: Read and display on Serial Monitor the conversion results of the AD7746 Data Registers.
+ * License: Copyright (c) 2020, The library is licensed under the MIT license.
+*/
+
 // The function allows to read only the content of the device's Data Registers:
 // Cap Data Register: H byte (AP=0x01), M byte (AP=0x02), L byte (AP=0x03).
 // VT Data Register: H byte (AP=0x04), M byte (AP=0x05), L byte (AP=0x06).
 // All three bytes of the data register are read sequentially 
 // by using the address pointer auto-increment feature.
-
-// 2020-08-10 by Mattia Devangelio <mattdev6@gmail.com>
 
 void dataRegisters()
 {
@@ -33,8 +38,8 @@ void dataRegisters()
   readvalue[0]=((unsigned long)d[0]<<16)+((unsigned long)d[1]<<8)+(unsigned long)d[2];
   readvalue[1]=((unsigned long)d[3]<<16)+((unsigned long)d[4]<<8)+(unsigned long)d[5];
   
-  data[0]=(((float)readvalue[0]-powf(2,23))/powf(2,23))*(float)caprange; //Capacitance reading
-  data[1]=(((float)readvalue[1]-powf(2,23))/powf(2,23))*(float)vtrange; //Voltage reading
+  data[0]=(((float)readvalue[0]-powf(2,23))/powf(2,23))*(float)caprange; //Capacitance reading (returned in pF)
+  data[1]=(((float)readvalue[1]-powf(2,23))/powf(2,23))*(float)vtrange; //Voltage reading (returned in V)
 
   Serial.print(data[0],5);
   Serial.print(" pF");
